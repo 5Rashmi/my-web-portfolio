145238e88d9b855274bc0e75625bcd1a4a7dc728
Rashmi <rr456rashmi@gmail.com>
2025-06-18 00:49:45 +0530

    page not found page created

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import Toolbar from "./ui/Toolbar/Toolbar";
 import MessageWindow from "./ui/Window/MessageWindow";
 import { Toaster } from "react-hot-toast";
 import LoadingScreen from "./ui/loading/LoadingScreen";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NotFound from "./pages/NotFound";
 
 function App() {
   const [bg, setBg] = useState(bgImg[0]);
@@ -21,34 +23,46 @@ function App() {
   }, []);
 
   return (
-    <Layout bg={bg}>
-      {loading ? (
-        <LoadingScreen />
-      ) : (
-        <>
-          <Home />
-          <Toaster position="bottom-right" />
-          {showSettings && (
-            <SettingsWindow
-              setBg={setBg}
-              onClose={() => setShowSettings(false)}
-            />
-          )}
-          {showMessage && (
-            <MessageWindow onClose={() => setShowMessage(false)} />
-          )}
+    <Router>
+      <Layout bg={bg}>
+        {loading ? (
+          <LoadingScreen />
+        ) : (
+          <>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
 
-          <Toolbar>
-            <button className="w-10 h-10" onClick={() => setShowSettings(true)}>
-              <img src="/icons/refreshcl_by_tpdk/PNG/Office/wheel-1.png" />
-            </button>
-            <button className="w-10 h-10" onClick={() => setShowMessage(true)}>
-              <img src="/icons/ui-icons/chat.png" />
-            </button>
-          </Toolbar>
-        </>
-      )}
-    </Layout>
+            <Toaster position="bottom-right" />
+            {showSettings && (
+              <SettingsWindow
+                setBg={setBg}
+                onClose={() => setShowSettings(false)}
+              />
+            )}
+            {showMessage && (
+              <MessageWindow onClose={() => setShowMessage(false)} />
+            )}
+
+            <Toolbar>
+              <button
+                className="w-10 h-10"
+                onClick={() => setShowSettings(true)}
+              >
+                <img src="/icons/refreshcl_by_tpdk/PNG/Office/wheel-1.png" />
+              </button>
+              <button
+                className="w-10 h-10"
+                onClick={() => setShowMessage(true)}
+              >
+                <img src="/icons/ui-icons/chat.png" />
+              </button>
+            </Toolbar>
+          </>
+        )}
+      </Layout>
+    </Router>
   );
 }
 
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,13 @@
+const NotFound = () => {
+  return (
+    <div className="text-center text-white mt-10">
+      <h1 className="text-4xl font-bold">404</h1>
+      <p className="text-lg mt-2">Page not found.</p>
+      <a href="/" className="text-blue-400 underline mt-4 block">
+        Go Home
+      </a>
+    </div>
+  );
+};
+
+export default NotFound;
