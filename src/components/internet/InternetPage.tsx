aaceba6cfd22465281767d5b9d09c89eadbb63dd
Rashmi <rr456rashmi@gmail.com>
2025-07-12 20:06:24 +0530

    loading added for internet load

--- src/components/internet/InternetPage.tsx
+++ src/components/internet/InternetPage.tsx
@@ -6,20 +6,28 @@ import SearchResults from "./SearchResults";
 const InternetPage = () => {
   const [results, setResults] = useState<any[]>([]);
   const [hasSearched, setHasSearched] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const handleSearch = async (query: string) => {
+    setLoading(true);
     const data = await getInternetApi(query);
     console.log("FULL API RESPONSE:", data);
 
     const links = data?.organic_results || [];
     setResults(links);
     setHasSearched(true);
+    setLoading(false);
   };
 
   return (
     <div className="w-full mx-auto px-8 py-2 overflow-y-auto">
       <SearchBar onSearch={handleSearch} />
-      {hasSearched && <SearchResults results={results} />}
+      {loading && (
+        <div className="text-center mt-4 text-lg font-medium text-gray-600 animate-pulse">
+          Loading...
+        </div>
+      )}
+      {!loading && hasSearched && <SearchResults results={results} />}
     </div>
   );
 };
